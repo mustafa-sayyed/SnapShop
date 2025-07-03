1b421e33737bb9a39633e2212cd55403eb9f3b91
Mustafa Sayyed <mustafasayyed2429@gmail.com>
2025-07-04 00:06:41 +0530

    Resolve issue of MONGO URI

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const uri = `${process.env.MONGO_ALT_URI}/${process.env.DB_NAME}`;
+    const uri = `${process.env.MONGO_URI}/${process.env.DB_NAME}`;
     const connectionInstance = await mongoose.connect(uri);
     console.log(
       `MongoDB Connected: DB: ${connectionInstance.connection.name}, host: ${connectionInstance.connection.host}`
